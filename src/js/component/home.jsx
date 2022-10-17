2d4c74e506298a780a7e9f662bb1f7eb7f730aef
federicaramirez <federicaaramirez@gmail.com>
2022-10-17 21:19:34 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+    const [tarea, setTarea] = useState("")
+    const [listaTareas, setListaTareas] = useState([])
+    const [listaVacia, setListaVacia] = useState ("No hay tareas")
+
+    function subirTarea(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+
+            setListaTareas([
+                ...listaTareas,
+                tarea
+            ]); // suma la nueva sin borrar lo anterior
+            console.log(listaTareas);
+            setTarea("");
+            setListaVacia("")
+        } 
+
+    }
+
+
+    function eliminarElemento(id) {
+        let sinEliminado = [];
+        sinEliminado = listaTareas.filter((item,index) => {
+            if (index !== id)  {
+                return item
+            } 
+            if (listaVacia == "" && listaTareas.length == 1 ) {
+                setListaVacia("No hay tareas")
+            } 
+        } ) 
+        setListaTareas(sinEliminado);
+    }
+      
+
+
+    return (
+        <div>
+            <div>
+            <h1>Tareas</h1>
+            </div>
+            <div>
+                <input type="text" className="form-control justify-content-center" id="formGroupExampleInput" placeholder="Escribir tarea"
+                    value={tarea}
+                    onChange={
+                        (e) => setTarea(e.target.value)
+                    }
+                    onKeyDown={subirTarea}/>
+            </div>
+                <ul className="list-group list-group-flush">
+					{listaTareas.map((item, id) => 
+                    <li className = "list-group-item hidden" key={id}>{item} 
+                    <button type="button" className="btn btn-outline-primary float-end" 
+                    onClick={()=> eliminarElemento(id)}>x
+                    </button>
+                    </li> ) }
+				</ul>
+                {listaVacia}
+
+            </div>
+    );
 };
 
 export default Home;
